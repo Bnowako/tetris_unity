9003bf98ff7155de94dd0b733f1635f25d555e93
Bnowako <bnowakowski98@gmail.com>
2021-02-04 00:23:23 +0100

    add_grid_collisions_rotation

--- Tetris_zaliczenie/Assets/GameLogic.cs
+++ Tetris_zaliczenie/Assets/GameLogic.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-
     public GameObject[] tetrominos;
 
-    // Width and haight is in units. 1 unit = 56px
+    //Width and haight is in units. 1 unit = 56px
     public static int game_board_width = 10;
     public static int game_board_height = 20;
 
     // Declare grid that will corespond to our game board status.
-    private new Transform[,] grid;
-   
+    public static new Transform[,] grid;
+
+    public static bool next_piece_needed = false;
 
     void Start()
     {
+        grid = new Transform[game_board_width, game_board_height];
         InitializeNextTetromino();
-        grid = new Transform[game_board_width,game_board_height];
-        
     }
 
     void Update()
     {
+        if (next_piece_needed)
+        {
+           InitializeNextTetromino();
+            next_piece_needed = false;
+            TetrominoMovement.next_piece_needed = false;
+        }
+
     }
 
+
+ 
 
     void InitializeNextTetromino()
     {
@@ -36,41 +44,78 @@ public class GameLogic : MonoBehaviour
         // https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
         // Quaternion.identity - no rotation
 
-        int spawn_x = (int) (game_board_width / 2);
-        int spawn_y = game_board_height;
+        int spawn_x = (int)(GameLogic.game_board_width / 2);
+        int spawn_y = GameLogic.game_board_height;
 
         Instantiate(tetrominos[i], new Vector3(spawn_x, spawn_y, 0), Quaternion.identity);
     }
 
 
- 
 
-    public static bool IsGridPosValid(Vector2 position,char direction)
+
+    public static bool IsGridPosValid(Vector2 position, char direction)
     {
+        
+
         int temp_pos_x = (int)position.x;
         int temp_pos_y = (int)position.y;
-        Debug.Log(position.x);
+        //Debug.Log(position.x);
         //Debug.Log(position.y);
-        if (direction == 'L')
+
+        if (direction == 'L') temp_pos_x--;
+        if (direction == 'R') temp_pos_x++;
+        if (direction == 'D') temp_pos_y--; 
+
+
+        bool valid_movement = temp_pos_x >= 0 && temp_pos_x < game_board_width && temp_pos_y >= 0;
+
+
+        if (!valid_movement)
         {
-            temp_pos_x --;
-        }
-        else if (direction == 'R')
-        {
-            temp_pos_x ++;
-        }
-        else if (direction == 'D')
-        {
-            temp_pos_y --;
+            return false;
         }
 
-        bool valid_movement = temp_pos_x >= 0 && temp_pos_x < game_board_width && temp_pos_y > 0;
+        bool free_space = IsSpaceFree(temp_pos_x, temp_pos_y);
+
+        if (direction == 'D' && !free_space)
+        {
+            TetrominoMovement.next_piece_needed = true;
+            return false;
+
+        }
+
+        valid_movement = valid_movement && free_space;
+
         return valid_movement;
+    }
+
+
+
+    public static bool IsSpaceFree(int x,int y)
+    {
+        if (y < 20)
+        {
+            if (grid[x, y])
+            {
+                return false;
+
+            }
+
+            return true;
+        }
+        return true;
+        
+
     }
 
     public static Vector2 RoundVector(Vector2 vector)
     {
         return new Vector2(Mathf.Round(vector.x), Mathf.Round(vector.y));
     }
+
+
 }
+
+    
+
 
--- Tetris_zaliczenie/Assets/TetrominoMovement.cs
+++ Tetris_zaliczenie/Assets/TetrominoMovement.cs
@@ -4,42 +4,67 @@ using UnityEngine;
 
 public class TetrominoMovement : MonoBehaviour
 {
-    bool validMovement = false;
+    //GameObject[] tetrominos;
+
+
+    bool valid_movement = false;
+    float last_piece_fall = 0;
+
+    float falling_speed = 1;
+    public static bool next_piece_needed = false;
     // Start is called before the first frame update
     void Start()
     {
+        //InitializeNextTetromino();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (next_piece_needed)
+        {
+            StopTetromino();
+
+        }
         
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && validateMovement('L'))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && ValidateMovement('L'))
         {
             transform.position += new Vector3(-1, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && validateMovement('R'))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && ValidateMovement('R'))
         {
             transform.position += new Vector3(1, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && validateMovement('D'))
+        else if ((Input.GetKeyDown(KeyCode.DownArrow) || Time.time - last_piece_fall >= falling_speed) && ValidateMovement('D'))
         {
             transform.position += new Vector3(0, -1, 0);
+            last_piece_fall = Time.time;
+        }
+        else if(Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            RotatePieceIfPossible();
         }
         
-
+        
 
 
        
     }
-    bool validateMovement(char direction )
+    bool ValidateMovement(char direction )
     {
+
         foreach (Transform child in transform)
         {
+
+            if(child.position.y == 0)
+            {
+                next_piece_needed = true;
+            }
+
             Vector2 vector = GameLogic.RoundVector(child.position);
-            validMovement = GameLogic.IsGridPosValid(vector,direction);
-            if (!validMovement)
+            valid_movement = GameLogic.IsGridPosValid(vector,direction);
+            if (!valid_movement)
             {
                 return false;
             }
@@ -48,4 +73,48 @@ public class TetrominoMovement : MonoBehaviour
         return true;
 
     }
+
+    void RotatePieceIfPossible()
+    {
+        transform.Rotate(0, 0, -90);
+
+        foreach (Transform child in transform)
+        {
+            Vector2 vector = GameLogic.RoundVector(child.position);
+            valid_movement = GameLogic.IsGridPosValid(vector, 'O');
+            if (!valid_movement)
+            {
+                transform.Rotate(0, 0, 90);
+                return;
+            }
+        }
+        return;
+
+    }
+
+    void StopTetromino()
+    {
+        AddTetrominoToGrid();
+        GameLogic.next_piece_needed = true;
+        enabled = false;
+    }
+
+    void AddTetrominoToGrid()
+    {
+        foreach(Transform child in transform)
+        {
+     
+
+            Vector2 vector = GameLogic.RoundVector(child.position);
+            int x_pos = (int)vector.x;
+            int y_pos = (int)vector.y;
+
+   
+            GameLogic.grid[x_pos,y_pos] = child;
+        }
+    }
+
+
+ 
+
 }
