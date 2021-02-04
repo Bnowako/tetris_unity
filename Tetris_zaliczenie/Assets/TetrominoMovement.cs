2086fd69a0131154258e0c8cd91bb39eae565a57
Bnowako <bnowakowski98@gmail.com>
2021-02-04 02:49:34 +0100

    add_scoring_game_over_and_comments

--- Tetris_zaliczenie/Assets/TetrominoMovement.cs
+++ Tetris_zaliczenie/Assets/TetrominoMovement.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class TetrominoMovement : MonoBehaviour
 {
-    //GameObject[] tetrominos;
-
-
+    
     bool valid_movement = false;
+
+    // Falling time management
     float last_piece_fall = 0;
 
+    // 1s For each fall
     float falling_speed = 1;
-    public static bool next_piece_needed = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //InitializeNextTetromino();
 
-    }
+    public static bool next_piece_needed = false;
+  
 
     // Update is called once per frame
     void Update()
     {
+
         if (next_piece_needed)
         {
             StopTetromino();
 
         }
-        
+
+        // Handling input for game
         if (Input.GetKeyDown(KeyCode.LeftArrow) && ValidateMovement('L'))
         {
             transform.position += new Vector3(-1, 0, 0);
@@ -46,23 +45,21 @@ public class TetrominoMovement : MonoBehaviour
             RotatePieceIfPossible();
         }
         
-        
-
-
-       
     }
+
+    
     bool ValidateMovement(char direction )
     {
 
         foreach (Transform child in transform)
         {
+            Vector2 vector = GameLogic.RoundVector(child.position);
 
-            if(child.position.y == 0)
+            if(vector.y == 0)
             {
                 next_piece_needed = true;
             }
 
-            Vector2 vector = GameLogic.RoundVector(child.position);
             valid_movement = GameLogic.IsGridPosValid(vector,direction);
             if (!valid_movement)
             {
@@ -76,13 +73,16 @@ public class TetrominoMovement : MonoBehaviour
 
     void RotatePieceIfPossible()
     {
+        Debug.Log(gameObject.name);
         transform.Rotate(0, 0, -90);
 
         foreach (Transform child in transform)
         {
             Vector2 vector = GameLogic.RoundVector(child.position);
             valid_movement = GameLogic.IsGridPosValid(vector, 'O');
-            if (!valid_movement)
+
+            // Little trick :D with gameObject.name
+            if (!valid_movement || gameObject.name == "O_Tetromino(Clone)") 
             {
                 transform.Rotate(0, 0, 90);
                 return;
@@ -92,6 +92,7 @@ public class TetrominoMovement : MonoBehaviour
 
     }
 
+ 
     void StopTetromino()
     {
         AddTetrominoToGrid();
@@ -103,14 +104,16 @@ public class TetrominoMovement : MonoBehaviour
     {
         foreach(Transform child in transform)
         {
-     
 
             Vector2 vector = GameLogic.RoundVector(child.position);
             int x_pos = (int)vector.x;
             int y_pos = (int)vector.y;
 
-   
-            GameLogic.grid[x_pos,y_pos] = child;
+            if (y_pos < GameLogic.game_board_height)
+            {
+                GameLogic.grid[x_pos, y_pos] = child;
+            }
+
         }
     }
 
